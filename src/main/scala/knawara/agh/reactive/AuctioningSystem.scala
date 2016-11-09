780bb3d34271e9bddf9bbdf4d09c80ef2e9b3a94
BlueEyedHush <blueeyedhush@gmail.com>
2016-11-10 00:17:14 +0100

    simple bidding logic

--- src/main/scala/knawara/agh/reactive/AuctioningSystem.scala
+++ src/main/scala/knawara/agh/reactive/AuctioningSystem.scala
@@ -2,6 +2,22 @@ package knawara.agh.reactive
 
 import akka.actor.{ActorRef, Props, ActorSystem, Actor}
 
+import scala.util.Random
+
+/* todo
+- bid using scheduler, with delay
+- bid fixed price
+- bid random price
+- buyer - bid on list of auctions
+- send back current price
+- auction duration
+- highest bidder
+- handle bidder destruction
+ */
+
+case class PlaceBid(val price: Long)
+case class BidTooSmall()
+
 class AuctioningSystem extends Actor {
   val auction = context.actorOf(Props[Auction])
   val buyer = context.actorOf(Buyer.props(auction))
@@ -16,15 +32,23 @@ object Buyer {
 }
 
 class Buyer(auction: ActorRef) extends Actor {
-  auction ! "hello world"
+  auction ! PlaceBid(Random.nextInt())
 
   override def receive = {
+    case BidTooSmall() => println("Bid was too small")
     case _ @ msg => println(s"Buyer got new message: ${msg.toString}")
   }
 }
 
 class Auction extends Actor {
+  var price = 0L
+
   override def receive = {
+    case PlaceBid(offeredPrice) => {
+      println(s"Bid received: $offeredPrice")
+      if(offeredPrice > price) price = offeredPrice
+      else sender() ! BidTooSmall()
+    }
     case _ @ msg => println(s"Auction got new message: ${msg.toString}")
   }
 }
