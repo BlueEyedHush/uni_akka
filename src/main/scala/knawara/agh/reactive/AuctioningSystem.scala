67f50b50f9652e4e7f05559836d15200b31ed5cf
BlueEyedHush <blueeyedhush@gmail.com>
2016-11-10 13:14:03 +0100

    adding more buyers

--- src/main/scala/knawara/agh/reactive/AuctioningSystem.scala
+++ src/main/scala/knawara/agh/reactive/AuctioningSystem.scala
@@ -8,7 +8,7 @@ class AuctioningSystem extends Actor {
   val log = Logging(context.system, this)
 
   val auction = context.actorOf(AuctionActor.props(5 seconds), "auction")
-  val buyer = context.actorOf(BuyerActor.props(auction), "buyer")
+  Set(1,2,3).foreach(id => context.actorOf(BuyerActor.props(auction), s"buyer-$id"))
 
   override def receive = {
     case _ @ msg => log.debug(s"AuctionSystem got new message: ${msg.toString}")
