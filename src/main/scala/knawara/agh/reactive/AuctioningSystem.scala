3017901008e0648e12092ea4d712985543cdd17e
BlueEyedHush <blueeyedhush@gmail.com>
2016-11-10 11:07:35 +0100

    Auction FSM skeleton

--- src/main/scala/knawara/agh/reactive/AuctioningSystem.scala
+++ src/main/scala/knawara/agh/reactive/AuctioningSystem.scala
@@ -1,6 +1,6 @@
 package knawara.agh.reactive
 
-import akka.actor.{ActorRef, Props, ActorSystem, Actor}
+import akka.actor._
 
 import scala.util.Random
 
@@ -15,6 +15,7 @@ import scala.util.Random
 case class BidTick()
 case class PlaceBid(val price: Long)
 case class BidTooSmall()
+case class Relist()
 
 class AuctioningSystem extends Actor {
   val auction = context.actorOf(Props[Auction])
@@ -46,16 +47,50 @@ class Buyer(auction: ActorRef) extends Actor {
   }
 }
 
-class Auction extends Actor {
-  var price = 0L
+object Auction {
+  /* in-state messages */
+  val BID_TIMER_EXPIRED = "bte"
+  val DELETE_TIMER_EXPIRED = "dte"
+}
 
-  override def receive = {
-    case PlaceBid(offeredPrice) => {
-      println(s"Bid received: $offeredPrice")
-      if(offeredPrice > price) price = offeredPrice
-      else sender() ! BidTooSmall()
-    }
-    case _ @ msg => println(s"Auction got new message: ${msg.toString}")
+/* states */
+sealed trait AuctionState
+case object AuctionCreated extends AuctionState
+case object Ignored extends AuctionState
+case object Activated extends AuctionState
+case object Sold extends AuctionState
+case object Dead extends AuctionState
+
+/* data */
+case class AuctionData(val price: Long = 0L, val buyer: Option[ActorRef] = None)
+
+class Auction extends FSM[AuctionState, AuctionData] {
+  startWith(AuctionCreated, AuctionData())
+
+  when(AuctionCreated) {
+    case Event(PlaceBid(_), _) => goto(Activated)
+    case Event(Auction.BID_TIMER_EXPIRED, _) => goto(Ignored)
+  }
+
+  when(Ignored) {
+    case Event(Auction.DELETE_TIMER_EXPIRED, _) => goto(Dead)
+    case Event(Relist(), _) => goto(Activated)
+  }
+
+  when(Activated) {
+    case Event(PlaceBid(_), _) => goto(Activated)
+  }
+
+  when(Sold) {
+    case Event(Auction.BID_TIMER_EXPIRED, _) => goto(Sold)
+  }
+
+  onTransition {
+    case _ -> AuctionCreated => println("setting bid timer")
+    case _ -> Dead => println("cleanup auction actor")
+    case _ -> Ignored => println("set delete timer")
+    case _ -> Activated => println("validate bid, preventing transition if needed")
+    case _ -> Sold => println("set delete timer & notify buyer")
   }
 }
 
