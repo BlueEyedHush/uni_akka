7497d6ccae7f192f842ab01028a16bf2bef3d92f
BlueEyedHush <blueeyedhush@gmail.com>
2016-12-09 00:25:12 +0100

    added some tests for AuctionSearch

---
+++ src/test/scala/knawara/agh/reactive/AuctionSearchTests.scala
@@ -0,0 +1,55 @@
+package knawara.agh.reactive
+
+import akka.actor.ActorSystem
+import akka.testkit.{TestActorRef, TestProbe, TestFSMRef}
+import knawara.agh.reactive.AuctionSearch
+import org.scalatest.{FlatSpec, Matchers}
+
+class AuctionSearchTests extends FlatSpec with Matchers {
+  "AuctionSearch actor" should "return registered auction" in {
+    val ta = testActor()
+    val sender = TestProbe()
+    val auction = TestProbe()
+
+    ta ! AuctionSearch.Register(new AuctionTitle("auction"), auction.ref)
+    sender.send(ta, AuctionSearch.Lookup("auction"))
+    sender.expectMsgPF(){
+      case AuctionSearch.Result(_, auctionList) =>
+        assert(auctionList.head.equals(auction.ref))
+        assert(auctionList.size == 1)
+    }
+  }
+
+  it should "corrrectly repsond to partial query" in {
+    val ta = testActor()
+    val sender = TestProbe()
+    val auction = TestProbe()
+
+    ta ! AuctionSearch.Register(new AuctionTitle("auction"), auction.ref)
+    sender.send(ta, AuctionSearch.Lookup("uct"))
+    sender.expectMsgPF(){
+      case AuctionSearch.Result(_, auctionList) =>
+        assert(auctionList.head.equals(auction.ref))
+        assert(auctionList.size == 1)
+    }
+  }
+
+  it should "return nothing when querying non-existent auction" in {
+    val ta = testActor()
+    val sender = TestProbe()
+    val auction = TestProbe()
+
+    ta ! AuctionSearch.Register(new AuctionTitle("auction"), auction.ref)
+    sender.send(ta, AuctionSearch.Lookup("notpresent"))
+    sender.expectMsgPF(){
+      case AuctionSearch.Result(_, auctionList) =>
+        assert(auctionList.isEmpty)
+    }
+  }
+
+  implicit val system = ActorSystem("test")
+
+  private def testActor() = {
+    TestActorRef(new AuctionSearch.Actor)
+  }
+}
